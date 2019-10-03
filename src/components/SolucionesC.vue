24487949550d62ac6bb80c7378756a1b961a7d49
albertoramil <alberto.ramil@norvoz.es>
2019-10-03 16:06:40 +0200

    mounted en el data carga solo, pero una async en el data falla

--- src/components/SolucionesC.vue
+++ src/components/SolucionesC.vue
@@ -1,12 +1,17 @@
 <template >
   <div>
-    <v-btn outlined large fab v-on:click="consulta"></v-btn>
-      {{users}}
-      <br>      <br>
-      <br>
     <v-btn outlined large fab v-on:click="aconsulta"></v-btn>
+     
+      {{ausers}}
 
-            {{ausers}}
+      <template>
+        <v-data-table
+          :headers="cabecera2"
+          :items="info"
+          :items-per-page="5"
+          class="elevation-1"
+        ></v-data-table>
+      </template>
 
   </div>
 </template>
@@ -16,35 +21,52 @@ export default {
   name: 'app',
   data () {
     return {
-      users:null,
-      ausers:null
-
+      users:[],
+      ausers:null,
+      info:[],    
+      cabecera2: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Título', value: 'title' },
+          { text: 'Contenido', value: 'body' },
+        ]
     }
+    
+  }, mounted () {
+    console.log ("entra en el mounted")
+    this.$axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => (this.info = response.data))
   },
+  
+  
+  
+
   methods: {
     consulta: function () {
       console.log("sdfsdfs")
-      const baseURI = 'https://jsonplaceholder.typicode.com/users'
-        this.$http.get(baseURI)
+      const baseURI = 'https://jsonplaceholder.typicode.com/posts'
+      this.$http.get(baseURI)
       .then((result) => {
-        this.users = result.data
+      this.users = result.data
       })
-    }, 
-        async aconsulta() {
+    },  
+     async aconsulta() {
       try {
-              const baseURI = 'https://jsonplaceholder.typicode.com/posts6'
-              let respuesta=await this.$axios.get(baseURI)
-              this.ausers=respuesta
-        
+        const baseURI = 'https://jsonplaceholder.typicode.com/posts'
+        let respuesta=await this.$axios.get(baseURI)
+        this.ausers=respuesta
       } catch (error) {
         console.log("error")
         console.log(error)
         console.log("error")
-
-        
-      }
-      
-    }
+      }  
+    },
+ 
   }
 }
 </script>
